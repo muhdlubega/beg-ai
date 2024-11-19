b8f4e887dcd1e1fbb0c3084bb26cdce644522442
muhdlubega <111652389+muhdlubega@users.noreply.github.com>
2024-11-19 04:42:32 +0000

    feat: authentication

--- src/lib/supaBaseClient.ts
+++ src/lib/supaBaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl =  'https://wibhzswcdtrttyhetahp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpYmh6c3djZHRydHR5aGV0YWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5OTAzNDEsImV4cCI6MjA0NzU2NjM0MX0.DnGN6culb5Vty6eiJ4KsOPCeBNk-6yaio9iKiEUUtls'
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
